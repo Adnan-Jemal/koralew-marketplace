c496f13cbf97802c8f14bba07e2c21da35da286f
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-10-27 15:52:24 +0300

    styled item card and my items page

--- app/account/my-items/page.tsx
+++ app/account/my-items/page.tsx
@@ -10,9 +10,19 @@ export default async function page() {
   const userItems = await getUserItems();
 
   return (
-    <div className="flex flex-wrap gap-6 items-center justify-around p-10">
-      {userItems?.map((item)=><ItemCard title={item.product.title} imageUrl={item.product_images.imageUrl} price={parseFloat(item.product.price)} condition={item.product.condition} />)}
-      {/* <ItemCard
+    <div className="p-10 gap-10 flex flex-col">
+      <h1 className="text-4xl capitalize font-semibold">Your Listed Items</h1>
+      <div className="flex flex-wrap gap-8 items-center justify-around ">
+        {userItems?.map((item) => (
+          <ItemCard
+            title={item.product.title}
+            imageUrl={item.product_images.imageUrl}
+            price={parseFloat(item.product.price)}
+            condition={item.product.condition}
+          />
+        ))}
+
+        {/* <ItemCard
         title="rolex luxury men's watch"
         imageUrl="https://firebasestorage.googleapis.com/v0/b/koralew-fb48a.appspot.com/o/images%2Fproducts%2F1004%2Fimage_0?alt=media&token=237e1622-b3ad-4cab-8b31-2a99c97d31b9"
         price="10"
@@ -36,6 +46,7 @@ export default async function page() {
         price="600000"
         condition="Slightly Used"
       /> */}
+      </div>
     </div>
   );
 }
--- components/ItemCard/ItemCard.tsx
+++ components/ItemCard/ItemCard.tsx
@@ -16,17 +16,14 @@ export default function ItemCard({
 }: propType) {
   return (
     <div className="flex flex-col bg-primary-foreground justify-between w-60 h-80 rounded-2xl overflow-hidden shadow-md cursor-pointer ">
-      <Image
-        className=" h-56 w-full object-cover select-none "
-        height={100}
-        width={100}
-        src={imageUrl}
-        alt="Item Image"
-      />
-      <div className="p-3 flex flex-col justify-between gap-3">
-        <h3 className="text-xl capitalize line-clamp-2">{title}</h3>
-        <span className="h-[2px] w-[90%] bg-secondary self-center"></span>
-        <div className="flex justify-around px-2 ">
+      <div className="  h-[60%] object-cover select-none ">
+        <Image height={100} width={100} src={imageUrl} alt="Item Image" className=" h-full w-full object-cover" />
+      </div>
+
+      <div className="px-3 py-[10px] h-[40%] flex flex-col justify-between gap-3">
+        <h4 className="text-xl capitalize line-clamp-2 pb-0 z-10 ">{title}</h4>
+        <span className="h-[2px] p-0 m-0 w-[90%] bg-secondary self-center"/>
+        <div className="flex justify-around  px-2  ">
           <p className="font-bold text-xl ">${price.toFixed()}</p>
           <span className="w-[2px] bg-secondary"></span>
           <span className="bg-black bg-opacity-80 dark:bg-opacity-75 dark:bg-white dark:text-black text-white px-3 text-[14px] text-center max-h-fit py-[2px] rounded-md capitalize ">
