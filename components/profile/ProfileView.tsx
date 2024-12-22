935aecf49b4311f8b8444d75cb9aefc5b386a0d0
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-12-22 13:47:09 +0300

    removed unused imports

--- components/profile/ProfileView.tsx
+++ components/profile/ProfileView.tsx
@@ -1,5 +1,4 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { auth } from "@/auth";
 import UpdatePhotoBtn from "./UpdatePhotoBtn";
 import { Session } from "next-auth";
 
