de20d0a4100099144c45495a33f43fa4e005ad67
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-12-29 19:00:26 +0300

    made title responsive

--- app/account/my-items/loading.tsx
+++ app/account/my-items/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 export default function loading() {
   return (
     <div className="p-10 gap-10 flex flex-col">
-      <h1 className="text-4xl capitalize font-semibold">Your Listed Items</h1>
+      <h1 className="text-4xl capitalize font-semibold  text-center sm:text-start">Your Listed Items</h1>
       <div className="flex flex-wrap gap-8 items-center justify-around ">
         <Skeleton className="bg-primary-foreground w-64 h-80 rounded-2xl shadow-md cursor-pointer"/>
         <Skeleton className="bg-primary-foreground w-64 h-80 rounded-2xl shadow-md cursor-pointer"/>
